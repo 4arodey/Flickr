705d7dba76289ed67cb9212ad85cbea48e64abb4
Oleg Karpach <oleg.karpach@oxagile.com>
2021-05-27 16:59:21 +0300

    PP-1: add form validation functionality.

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IFieldBase } from 'src/app/interfaces/card.model';
-import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn, FormArray } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -13,6 +13,31 @@ export class FormComponent implements OnInit {
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.customerForm = this.fb.group({
+      textControl: ['', [Validators.required, Validators.minLength(3)]],
+      select: ['', Validators.required],
+      checkBox: [false, Validators.requiredTrue],
+      checkboxGroup: this.fb.group(
+        {
+          checkbox1: [false, Validators.requiredTrue],
+          checkbox2: [false, Validators.requiredTrue],
+        }
+      ),
+      gender: ['', Validators.required],
+      radioButton: true,
+      datepicker: ['', Validators.required],
+    });
   }
 
+  touchInput(controlName: string): boolean {
+    return (this.customerForm.get(controlName).touched ||
+      this.customerForm.get(controlName).dirty) &&
+      !this.customerForm.get(controlName).valid;
+  }
+
+  public save(): void {
+    // remove it after implementation of functionality
+    console.log(this.customerForm);
+    console.log('Saved:' + JSON.stringify(this.customerForm.value));
+  }
 }
